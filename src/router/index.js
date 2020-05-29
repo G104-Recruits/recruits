48091e92d28a239675a22bbb304745a7c96e6be8
PedroDuarte536 <pedro.dld536@gmail.com>
2020-05-29 19:29:44 +0100

    New Routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Profile from '../views/Profile.vue'
+import Search from '../views/Search.vue'
+import Project from '../views/Project.vue'
+import CreateProject from '../views/CreateProject.vue'
 
 Vue.use(VueRouter)
 
@@ -20,6 +23,21 @@ Vue.use(VueRouter)
     path: '/user/:id',
     name: 'Profile',
     component: Profile
+  },
+  {
+    path: '/search/:type',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/project/:id',
+    name: 'Project',
+    component: Project
+  },
+  {
+    path: '/create',
+    name: 'Create Project',
+    component: CreateProject
   }
 ]
 
